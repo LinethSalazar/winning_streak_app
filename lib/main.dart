8683f427de6d356e97490444cb2ad7714cb3ae7c
Lineth Salazar <lasalazardelabarra@upei.ca>
2026-03-25 20:59:08 -0300

    Initial comit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:winning_streak_app/high_score/cubit/hs_toggle_cubit.dart';
+import 'package:winning_streak_app/high_score/cubit/local_high_score_cubit.dart';
+import 'package:winning_streak_app/high_score/cubit/global_high_score_cubit.dart';
+import 'package:winning_streak_app/high_score/database/local_high_score_database.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+
+
+import 'app.dart';
+import 'high_score/database/global_high_score_database.dart';
+import 'streak_observer.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await LocalHighScoreDatabase.init();
+  await Firebase.initializeApp(
+    name: 'cs3130-winningstreak',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //FirebaseAuth auth = FirebaseAuth.instance;
+  /*await FirebaseAppCheck.instance.activate();
+  String? token = await FirebaseAppCheck.instance.getToken();
+  print("token: $token");
+  await FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true);
+
+  FirebaseAppCheck.instance.onTokenChange.listen((token) {
+    print("token: $token");
+  });*/
+
+  Bloc.observer = StreakObserver();
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<HSToggleCubit>(
+          create: (_) => HSToggleCubit(HSToggleCubit.LOCAL),
+        ),
+        BlocProvider<LocalHighScoreCubit>(
+          create: (_) => LocalHighScoreCubit(LocalHighScoreDatabase()),
+        ),
+        BlocProvider<GlobalHighScoreCubit>(
+          create: (_) => GlobalHighScoreCubit(GlobalHighScoreDatabase()),
+        ),
+      ],
+      child: StreakApp(),
+    ),
+  );
+}
---
+++ lib/streak/view/streak_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:winning_streak_app/auth/cubit/authorization_cubit.dart';
+import 'package:winning_streak_app/multiple_choice_question/multiple_choice_question.dart';
+
+import '../streak.dart';
+import 'streak_view.dart';
+
+/// Stateless widget responsible for providing StreakCubit to StreakView
+class StreakPage extends StatelessWidget {
+  const StreakPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+        BlocProvider<StreakCubit>(
+          create: (_) => StreakCubit(),
+        ),
+        BlocProvider<QuestionCubit>(
+          create: (_) => QuestionCubit(RandomQuestionFetch()),
+        ),
+        BlocProvider<AuthorizationCubit>(
+          create: (_) => AuthorizationCubit(),
+        )
+      //provide the authorization cubit
+      ],
+      child: StreakView(),
+    );
+  }
+}
